5ad1ba1a79bae9dbd499e376f7cf9a6dd7ea0f4b
Maratib Ali Khan <maratib@gmail.com>
2021-07-25 02:44:27 +0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+<template>
+  <div class="">
+    <h1 class="">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import { useI18n } from 'vue-i18n';
+
+import { defineComponent, ref } from 'vue';
+import { useTheme } from '/@/composables';
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const { t, availableLocales, locale } = useI18n();
+
+    const toggleLocales = () => {
+      const locales = availableLocales;
+      locale.value =
+        locales[(locales.indexOf(locale.value) + 1) % locales.length];
+    };
+
+    const { isDark, toggleDark } = useTheme();
+
+    const show = ref(false);
+
+    setTimeout(() => {
+      show.value = true;
+    }, 1000);
+
+    return { locale, t, show, toggleLocales, isDark, toggleDark };
+  },
+});
+</script>
+
+<template>
+  <div class="hi">
+    <HelloWorld class="bye" :msg="t('hello') + ' 👋 !!'" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.hi {
+  background: green;
+  padding: 2em;
+  .bye {
+    background: blue;
+  }
+}
+</style>
